96e943bf66e64565b5cdda36432d5352cec6c3d4
VZarei <Engitrefinery@gmail.com>
2024-04-25 16:22:23 +0100

    some change on css

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# STATICS
+
+STATIC_URL = '/statics/'
+STATICSFILES_DIRS = [str(BASE_DIR.joinpath('statics'))]
